efc6c86068e4cd9328fe3e62a9a42b097af449c2
cinthia <cinthiago0@gmail.com>
2018-03-03 00:37:11 +0100

    script folder added

---
+++ Assets/Scripts/TestUnityEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(InputsEvents))]
+
+public class InputsEventsEditor : Editor {
+
+    SerializedProperty nature;
+    SerializedProperty trigger_nb;
+    SerializedProperty delay;
+
+    void OnEnable()
+    {
+        nature = serializedObject.FindProperty("nature");
+        trigger_nb = serializedObject.FindProperty("trigger_nb");
+        delay = serializedObject.FindProperty("delay");
+
+    }
+
+}

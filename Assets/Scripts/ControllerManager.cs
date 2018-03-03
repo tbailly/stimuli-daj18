0985990c5f122083690924be8be441bfcea9439f
thomasbaillysalins <thomasbaillysalins01@gmail.com>
2018-03-03 16:06:19 +0100

    readd vibration management

---
+++ Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class ControllerManager : MonoBehaviour {
+
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Find a PlayerIndex, for a single player game
+        // Will find the first controller that is connected ans use it
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                    setVibration(0.1f, 0);
+                }
+            }
+        }
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+    }
+
+    public void setVibration(float left, float right)
+    {
+        if (!playerIndexSet)
+            return;
+        GamePad.SetVibration(playerIndex, left, right);
+    }
+}
--- Assets/Scripts/ObjectFadeOnApproach.cs
+++ Assets/Scripts/ObjectFadeOnApproach.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ObjectFadeOnApproach : MonoBehaviour {
 
     public Player player;
+    public ControllerManager controllerManager;
+    public float minDistance;
     public float startFadeDistance;
     public float endFadeDistance;
 
@@ -20,6 +22,7 @@ public class ObjectFadeOnApproach : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float distance = Vector3.Distance(transform.position, player.transform.position);
+        
         if (distance < startFadeDistance && distance > endFadeDistance)
         {
             if (rend.enabled == false)
@@ -27,10 +30,17 @@ public class ObjectFadeOnApproach : MonoBehaviour {
             float alpha = (distance - endFadeDistance) / (startFadeDistance - endFadeDistance);
             Color oldColor = mat.color;
             mat.color = new Color(oldColor.r, oldColor.g, oldColor.b, alpha);
+            controllerManager.setVibration(1 - alpha, 1 - alpha);
         }
         else if (distance <= endFadeDistance && rend.enabled == true)
+        {
             rend.enabled = false;
+            controllerManager.setVibration(0, 0);
+        }
         else if (distance >= startFadeDistance && rend.enabled == false)
+        {
             rend.enabled = true;
+            controllerManager.setVibration(0, 0);
+        }
     }
 }

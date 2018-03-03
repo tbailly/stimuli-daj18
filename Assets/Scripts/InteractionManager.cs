0055121d3f41540e679160d5955657e7c3e139ab
cinthia <cinthiago0@gmail.com>
2018-03-03 20:26:25 +0100

    skybox - variation color script with fade

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightManager : MonoBehaviour {
+public class InteractionManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {

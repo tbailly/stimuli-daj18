3b60a4ef74f9a685662dcc51a3ee84e3ba5d12a1
Korin <bawathor@gmail.com>
2018-03-04 16:25:21 +0100

    Prefab final des post processing changes

--- Assets/Scripts/PPShifter_Interior.cs
+++ Assets/Scripts/PPShifter_Interior.cs
@@ -8,17 +8,13 @@ public class PPShifter_Interior : MonoBehaviour
 {
 
     public PostProcessingProfile PPPremiere_Phase;
-    public PostProcessingProfile PPInterior;
+    public PostProcessingProfile PPPhase2;
+    public PostProcessingProfile PPPhase3;
     public Player player;
     private bool Interior = false;
-
-    public void OnTriggerEnter(Collider col)
-
-    {
-        if (col.gameObject.tag == "Player")
-            Interior = !Interior;
-    }
-
+    private float TimeToShift = 0.0f;
+    public float TimePPPhase2 = 60.0f;
+    public float TimePPPhase3 = 120.0f;
 
     // Use this for initialization
     void Start()
@@ -28,8 +24,12 @@ public class PPShifter_Interior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Interior)
-            player.GetComponentInChildren<PostProcessingBehaviour>().profile = PPInterior;
+        TimeToShift += Time.deltaTime;
+
+        if (TimeToShift > TimePPPhase2 && TimeToShift < TimePPPhase3)
+            player.GetComponentInChildren<PostProcessingBehaviour>().profile = PPPhase2;
+        else if (TimeToShift > TimePPPhase3)
+            player.GetComponentInChildren<PostProcessingBehaviour>().profile = PPPhase3;
         else
             player.GetComponentInChildren<PostProcessingBehaviour>().profile = PPPremiere_Phase;
     }

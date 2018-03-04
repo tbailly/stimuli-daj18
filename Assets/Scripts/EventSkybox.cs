962ac95e03a658f099ead287488c09c185132cca
cinthia <cinthiago0@gmail.com>
2018-03-04 14:55:32 +0100

    can change skybox exposure

--- Assets/Scripts/EventSkybox.cs
+++ Assets/Scripts/EventSkybox.cs
@@ -6,21 +6,27 @@ public class EventSkybox : MonoBehaviour {
 
     public Material mat;   
     private Color baseColor;
-    public Color newColor;  
+    public Color newColor;
+    private float baseExposure;
+    public float newExposure;
     public float durationFade;
 
     private Color lerpedColor = Color.white;
+    private float lerpedExposure;
     private float t = 0f;  
 
     public void Start()
     {
-        baseColor = mat.GetColor("_Tint");     
-        
+        baseColor = mat.GetColor("_Tint");
+        baseExposure = RenderSettings.skybox.GetFloat("_Exposure");
     }
     private void Update()
     {                   
         lerpedColor = Color.Lerp(baseColor, newColor, t);
+        lerpedExposure = Mathf.Lerp(baseExposure, newExposure, t);
+
         t += Time.deltaTime / durationFade;
-        mat.SetColor("_Tint", lerpedColor);        
+        mat.SetColor("_Tint", lerpedColor);
+        RenderSettings.skybox.SetFloat("_Exposure", lerpedExposure);
     }  
 }

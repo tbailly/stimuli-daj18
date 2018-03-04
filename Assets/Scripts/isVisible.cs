4b0b635b2626534e53a7654834efd3563225ac22
Korin <bawathor@gmail.com>
2018-03-04 12:30:19 +0100

    Line de lumière qui disparaît

---
+++ Assets/Scripts/isVisible.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isVisible : MonoBehaviour {
+
+    public float DestructionTime = 2.0f;
+    float Time = 0.0f;
+  
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (gameObject.GetComponent<Renderer>().isVisible)
+        {
+            Debug.Log("Visible");
+            Time++;
+            if(Time == DestructionTime)
+            {
+                Destroy(gameObject);
+                Debug.Log("Disappearing");
+            }
+            
+        }
+        else
+        {
+            print("Not Visible");
+        }
+
+
+    }
+}

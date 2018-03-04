782400d8bbea13d80f6258d7f71fb6ed8cf2191f
Korin <bawathor@gmail.com>
2018-03-04 15:31:48 +0100

    Post Processing Shifter, plusieurs profils de post processing, nouvelles skybox, PREFAB DES SYSTEMES DE PARTICULES

---
+++ Assets/Scripts/PPShifter_Interior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+
+public class PPShifter_Interior : MonoBehaviour
+{
+
+    public PostProcessingProfile PPPremiere_Phase;
+    public PostProcessingProfile PPInterior;
+    public Player player;
+    private bool Interior = false;
+
+    public void OnTriggerEnter(Collider col)
+
+    {
+        if (col.gameObject.tag == "Player")
+            Interior = !Interior;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Interior)
+            player.GetComponentInChildren<PostProcessingBehaviour>().profile = PPInterior;
+        else
+            player.GetComponentInChildren<PostProcessingBehaviour>().profile = PPPremiere_Phase;
+    }
+}
+

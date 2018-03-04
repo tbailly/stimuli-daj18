324f5c4d3fbebb73722973943ac4257367fe18e5
thomasbaillysalins <thomasbaillysalins01@gmail.com>
2018-03-04 11:59:54 +0100

    add hub and beacon tweak

---
+++ Assets/Scripts/Phare.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phare : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("BeaconRotation");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    IEnumerator BeaconRotation()
+    {
+        while(true)
+        {
+            transform.Rotate(new Vector3(0, 0, 6));
+            yield return null;
+        }
+    }
+}
